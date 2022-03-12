ad99b03fdda2f53c87bc53fb18ec8f329f441d0f
hugosenari <hugosenari@gmail.com>
2022-03-12 02:08:25 -0300

    chore: re index winres

---
+++ v0_1_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Windows resource file generator'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-winres-v0_1_0.flake = false;
+  inputs.src-winres-v0_1_0.owner = "codehz";
+  inputs.src-winres-v0_1_0.ref   = "refs/tags/v0.1.0";
+  inputs.src-winres-v0_1_0.repo  = "nim-winres";
+  inputs.src-winres-v0_1_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-winres-v0_1_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-winres-v0_1_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
